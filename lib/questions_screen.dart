66d780d925647f99775ee539fc7f585b59d01d6d
Nititanit <nnititanit@gmail.com>
2025-01-24 11:53:37 +0700

    update

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/answer_button.dart';
 import 'package:myapp/data/quizz.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,10 +13,36 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-  final currentQuestion = question[0];
+  int currentQuestionIndex = 0;
+
+  void answerQuestiong(){
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+  final List<String> selectedAnswers = [];
+
+  void answerQuestion(String selectedAnswer) {
+    setState(() {
+      selectedAnswer.add(selectedAnswer);
+      if (currentQuestionIndex < question.length - 1) {
+        currentQuestionIndex++;
+      } else {
+       Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              ResultScreen(selectedAnswers: selectedAnswers),
+        ),
+       );
+      }
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = question[currentQuestionIndex];
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -23,27 +51,33 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               colors: [Colors.purple, Colors.deepPurple],
             ),
           ),
-          child: Center(
-            child: Container(
-              margin: const EdgeInsets.all(40),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    currentQuestion.question,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+          child: SizedBox(
+            child: Center(
+              child: Container(
+                margin: const EdgeInsets.all(40),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      currentQuestion.question,
+                      style: GoogleFonts.lato(
+                        color: const Color.fromARGB(255,201,153,251),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 30),
-                  ...currentQuestion.answers.map(
-                    (answer) {
-                      return AnswerButton(answer);
-                    },
-                  ),
-                ],
+                   
+                   const SizedBox(height: 30),
+                    ...currentQuestion.getShuffledAnswers().map(
+                      (answer) {
+                        return AnswerButton(
+                        answer: answer,
+                        onTap: () => answerQuestion(answer));
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
